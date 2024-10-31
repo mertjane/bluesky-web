6384c92ec2a163451e73625445c020a729a934b3
unknown <mck0391@gmail.com>
2024-10-31 13:46:37 +0000

    fixing bugs

--- app/components/SliderSection.tsx
+++ app/components/SliderSection.tsx
@@ -24,7 +24,7 @@ const SliderSection = () => {
       </div>
 
       {/* Content */}
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 w-full max-w-7xl px-4 sm:px-6 lg:px-4">
+      <div className="absolute lg:top-1/2 top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/3 z-10 w-full max-w-7xl px-4 sm:px-6 lg:px-4">
         <div className="text-left">
           <h1 className="text-5xl lg:text-8xl w-min tracking-tight font-black text-white sm:text-5xl md:text-5xl">
             Blue Sky Alterations
@@ -32,8 +32,8 @@ const SliderSection = () => {
           <p className="mt-3 max-w-md text-base text-white sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
             Professional clothing alterations and repairs in Rottingdean
           </p>
-          <div className="mt-5 max-w-md sm:flex lg:justify-start sm:justify-center md:mt-8">
-            <div>
+          <div className="mt-5 lg:max-w-auto sm:flex lg:justify-start sm:justify-center md:mt-8 max-w-60">
+            <div className="">
               <Link
                 href="#contact"
                 className="flex items-center justify-center px-8 py-3 border-transparent text-base font-medium text-white bg-transparent border-2 border-white hover:bg-gray-50 hover:text-black md:py-4 md:text-lg md:px-10"
